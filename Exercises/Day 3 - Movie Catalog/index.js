27ae8997b76094cdec8dd207bdeb4a4693c2319e
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2023-11-22 20:16:26 -0400

    Add movie image display to the main page

--- Exercises/Day 3 - Movie Catalog/index.js
+++ Exercises/Day 3 - Movie Catalog/index.js
@@ -1,16 +1,11 @@
-var guilhermeMovieChoice =
-  "https://upload.wikimedia.org/wikipedia/en/8/8e/Dune_%282021_film%29.jpg";
-var atlasMovieChoice =
-  "https://m.media-amazon.com/images/M/MV5BY2JmMDJlMmEtYTk4OS00YWQ5LTk2NzMtM2M3NzhkMjI4MGJkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_.jpg";
-var dianaMovieChoice =
-  "https://m.media-amazon.com/images/M/MV5BNjMyMDBjMGUtNDUzZi00N2MwLTg1MjItZTk2MDE1OTZmNTYxXkEyXkFqcGdeQXVyMTQ5NjA0NDM0._V1_.jpg";
-var minervaMovieChoice =
-  "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p15766_p_v8_ab.jpg";
-var marianaMovieChoice =
-  "https://m.media-amazon.com/images/M/MV5BYThmYjJhMGItNjlmOC00ZDRiLWEzNjUtZjU4MjA3MzY0MzFmXkEyXkFqcGdeQXVyNTI4MjkwNjA@._V1_.jpg";
+var movieList = [
+  "https://upload.wikimedia.org/wikipedia/en/8/8e/Dune_%282021_film%29.jpg",
+  "https://m.media-amazon.com/images/M/MV5BY2JmMDJlMmEtYTk4OS00YWQ5LTk2NzMtM2M3NzhkMjI4MGJkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_.jpg",
+  "https://m.media-amazon.com/images/M/MV5BNjMyMDBjMGUtNDUzZi00N2MwLTg1MjItZTk2MDE1OTZmNTYxXkEyXkFqcGdeQXVyMTQ5NjA0NDM0._V1_.jpg",
+  "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p15766_p_v8_ab.jpg",
+  "https://m.media-amazon.com/images/M/MV5BYThmYjJhMGItNjlmOC00ZDRiLWEzNjUtZjU4MjA3MzY0MzFmXkEyXkFqcGdeQXVyNTI4MjkwNjA@._V1_.jpg",
+];
 
-document.write("<img src=" + guilhermeMovieChoice + ">");
-document.write("<img src=" + atlasMovieChoice + ">");
-document.write("<img src=" + dianaMovieChoice + ">");
-document.write("<img src=" + minervaMovieChoice + ">");
-document.write("<img src=" + marianaMovieChoice + ">");
+for (var i = 0; i < movieList.length; i++) {
+  document.write("<img src=" + movieList[i] + ">");
+}
